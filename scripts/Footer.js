8da90554beb52d8857252d1c72c7a36158b49c07
cooldrew4567 <coodrew4567@gmail.com>
2023-04-25 19:16:24 -0700

    h

--- scripts/Footer.js
+++ scripts/Footer.js
@@ -1,4 +1,4 @@
-<script>
-   let lastUpdated = new Date(document.lastModified); 
-   document.getElementById("lastUpdated").innerHTML = lastUpdated;
-</script>
+document.querySelector("#year").innerHTML = "&copy; 2023 | Andrew Stephen Jones | California";
+
+let lastUpdated = new Date(document.lastModified);
+document.getElementById("lastUpdated").textContent = lastUpdated;
